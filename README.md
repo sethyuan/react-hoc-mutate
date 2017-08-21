eaaf83a438a001cca7d3bf63c352757229462200
sethyuan <sethyuan@gmail.com>
2017-08-21 16:56:31 +0800

    feat: mutate implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-hoc-mutate
+
+HOC to help you mutate restful data.
+
+## Dev setup
+
+1. `yarn`
+
+### npm run tasks
+
+- **yarn build**: Transpile source code
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from "react"
+import timer from "react-hoc-timerfuncs"
+
+function displayName(Comp) {
+  return Comp.displayName || Comp.name || "Component"
+}
+
+function mutate({ name = "mutate", op }) {
+  return Comp => {
+    @timer
+    class Mutate extends React.Component {
+      static displayName = `Mutate(${displayName(Comp)})`
+
+      render() {
+        /* eslint-disable no-unused-vars */
+        const { ...attrs } = this.props
+        /* eslint-enable no-unused-vars */
+
+        const props = {
+          [name]: this.mutate,
+        }
+
+        return <Comp {...attrs} {...props} />
+      }
+
+      mutate = async (...args) => {
+        const { setTimeout, clearTimeout } = this.props
+
+        try {
+          this.loadTimer = setTimeout(mutate.openLoading, mutate.loadingWait)
+          const data = await op(...args)
+          clearTimeout(this.loadTimer)
+          return data
+        } catch (e) {
+          clearTimeout(this.loadTimer)
+          mutate.onError(e)
+          throw e
+        } finally {
+          mutate.closeLoading()
+        }
+      }
+    }
+    return Mutate
+  }
+}
+
+mutate.loadingWait = 0.1
+mutate.openLoading = () => {}
+mutate.closeLoading = () => {}
+mutate.onError = () => {}
+
+export default mutate
